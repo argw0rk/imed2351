161689db170bbda4c6ec74ed6ba2bcc01783af1b
Aracely <agarcia1141@gmail.com>
2026-08-31 20:29:54 -0500

    constant object

--- module2/app.js
+++ module2/app.js
@@ -1,11 +1,23 @@
 
-let x, y, z;
-x = 10;
-y =2;
-z = x + y;
+//new way of code
 
-/* This is a comment box*/
-let _mod = "module";
-let _x = 2;
-let _y = 8;
-console.log(box);
+let life = 100; // this is a player's life variable in a video game NUMBERS\
+ life = life - 10;
+
+
+let name = 'player one'; // this is a player's name variable in the game STRINGS
+
+
+//CONSTANT OBJECT
+/*Create an object*/
+const player = {type:"Fiat", model:"500", color:"white"};
+
+player.color = "red"; // Change a property:
+player.owner = "Aracely"; // property:
+document.getElementById("demo").innerHTML = "This player's name is " + player.owner;
+
+
+
+console.log(life);
+
+document.write('Module2');
