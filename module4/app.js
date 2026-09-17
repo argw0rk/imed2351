f3ec45fe5dbf713bbb4878e34090bfb7efa9c5f6
Aracely <agarcia1141@gmail.com>
2026-09-17 00:32:41 -0500

    adding mod 4

---
+++ module4/app.js
@@ -0,0 +1,10 @@
+const age = 20;
+
+if(age > 18) {
+    console.log("You are an adult.");
+} else if (age <15){
+    console.log("You are a child.");
+}
+else {
+    console.log("You are not an adult.");
+}
