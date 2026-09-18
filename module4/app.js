4a3ca8f29d6305f440cd44578cc705cbdae6b8a2
Aracely <agarcia1141@gmail.com>
2026-09-17 22:32:46 -0500

    Updated

--- module4/app.js
+++ module4/app.js
@@ -1,3 +1,5 @@
+//Conditional Statements
+
 const age = 20;
 
 if(age > 18) {
@@ -8,3 +10,37 @@ if(age > 18) {
 else {
     console.log("You are not an adult.");
 }
+
+const MyAge = 25;
+
+const yourAge = 30;
+
+console.log(MyAge + yourAge);
+
+const isAgeInteger = Number.isInteger(age);
+console.log(isAgeInteger);
+
+if (yourAge > MyAge) {
+    console.log("Your older than me.");
+}
+
+const day = "Monday";
+
+switch (day) {
+    case "Monday":
+        console.log("It is the start of the week.");
+        break;
+    case "Friday":
+        console.log("The weekend is almost here.");
+        break;
+    default:
+        console.log("It is another day of the week.");
+}
+
+//String Concatenation
+const name = "cely";
+const greeting = "Hello, my name is " + name + ".";
+console.log(greeting);
+
+const uppercaseName = name.toUpperCase();
+console.log(uppercaseName);
